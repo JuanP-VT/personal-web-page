87750bea30bbc34593f4fa8ba360989077308554
JuanP-VT <cv.juanp@gmail.com>
2022-06-16 16:36:43 -0600

    Added icons to navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,32 @@
 import React from "react";
-
 function NavBar() {
   return (
     <div id="NavBar">
       <div id="navBarName">Juan Pablo</div>
       <div id="navBarList">
-        <a href="asd">Home</a>
-        <a href="asd">About</a>
-        <a href="asd">Skills</a>
-        <a href="asd">Showcase</a>
-        <a href="asd">All Projects</a>
+        <div className="container">
+          <span className="material-symbols-outlined">home</span>
+
+          <a href="#Home">Home</a>
+        </div>
+        <div className="container">
+          <span className="material-symbols-outlined">person</span>
+          <a href="#About">About</a>
+        </div>
+        <div className="container">
+          <span className="material-symbols-outlined">construction</span>
+          <a href="#Skills">Skills</a>
+        </div>
+        <div className="container">
+          <span className="material-symbols-outlined">
+            temp_preferences_custom
+          </span>
+          <a href="#Showcase">Showcase</a>
+        </div>
+        <div className="container">
+          <span className="material-symbols-outlined">Folder</span>
+          <a href="#allProjects">All Projects</a>
+        </div>
       </div>
     </div>
   );
