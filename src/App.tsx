ec77e2999e21a6a6ead64a058f42e990b433505d
JuanP-VT <cv.juanp@gmail.com>
2022-06-16 12:18:54 -0600

    Added main NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from "react";
-import "./App.css";
+import "./styles/App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <div> Hello World!</div>;
+  return (
+    <div id="App">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div id="NavBar">
+      <div id="navBarName">Juan Pablo</div>
+      <div id="navBarList">
+        <a href="asd">Home</a>
+        <a href="asd">About</a>
+        <a href="asd">Skills</a>
+        <a href="asd">Showcase</a>
+        <a href="asd">All Projects</a>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
