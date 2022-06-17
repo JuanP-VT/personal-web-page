a46c97a36451d2b85490815836a809eca2658080
JuanP-VT <cv.juanp@gmail.com>
2022-06-17 12:24:27 -0600

    Added text animation to Home componenet

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 function Home() {
-  return <section id="Home">Home</section>;
+  return (
+    <section id="Home">
+      <div className="container">
+        <div className="wrapper">
+          <div className="static-txt">Hi !, I'm a</div>
+          <ul className="dynamic-txts">
+            <li>
+              <span>Former Industrial Engineer</span>
+            </li>
+            <li>
+              <span>Web Developer</span>
+            </li>
+            <li>
+              <span>Coder</span>
+            </li>
+            <li>
+              <span>Enthusiast</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
