911843fb0efa6677b0f3b1d5cfb976fe533d8acb
JuanP-VT <cv.juanp@gmail.com>
2022-06-17 13:23:50 -0600

    Added social tab to home component

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,5 +1,7 @@
 import React from "react";
-
+import LinkedIn from "../img/linkedin.png";
+import gitHub from "../img/github.png";
+import emial from "../img/email.png";
 function Home() {
   return (
     <section id="Home">
@@ -20,6 +22,26 @@ function Home() {
               <span>Passionate Learner</span>
             </li>
           </ul>
+        </div>
+      </div>
+      <div className="socials">
+        <div className="Scontainer linkedin">
+          <div className="text">LinkedIn</div>
+          <div className="iconContainer">
+            <img src={LinkedIn} alt="Linkedin icon" />
+          </div>
+        </div>
+        <div className="Scontainer github">
+          <div className="text">GitHub</div>
+          <div className="iconContainer">
+            <img src={gitHub} alt="Linkedin icon " />
+          </div>
+        </div>
+        <div className="Scontainer email">
+          <div className="text">Email</div>
+          <div className="iconContainer ">
+            <img src={emial} alt="Linkedin icon" />
+          </div>
         </div>
       </div>
     </section>
