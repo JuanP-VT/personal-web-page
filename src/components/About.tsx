380b60f4c73fe8baae6e8918d339445729374cdc
JuanP-VT <cv.juanp@gmail.com>
2022-06-20 12:58:54 -0600

    Added Section Title component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 function About() {
-  return <section id="About">About</section>;
+  return (
+    <section id="About">
+      <div className="sectionTitle">
+        <p>
+          <span className="alter">About</span> me
+        </p>
+      </div>
+    </section>
+  );
 }
 
 export default About;
