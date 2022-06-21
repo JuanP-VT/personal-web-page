fe2de6d1683e9d4b082b5de690184901824a4835
JuanP-VT <cv.juanp@gmail.com>
2022-06-21 15:38:08 -0600

    added skills section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,7 +1,37 @@
 import React from "react";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCode,
+  faWrench,
+  faBookOpenReader,
+} from "@fortawesome/free-solid-svg-icons";
 function Skills() {
-  return <section id="Skills">Skills</section>;
+  return (
+    <section id="Skills">
+      <div className="sectionTitle">
+        <p>
+          My
+          <span className="alter"> Skills</span>
+        </p>
+      </div>
+      <div className="container">
+        <div className="skillBox">
+          <FontAwesomeIcon icon={faCode} />
+          <p className="title">Languages</p>
+        </div>
+        <div className="skillBox">
+          <FontAwesomeIcon icon={faBookOpenReader} />
+
+          <p className="title">Frameworks/Libraries/Databases</p>
+        </div>
+        <div className="skillBox">
+          <FontAwesomeIcon icon={faWrench} />
+
+          <p className="title">Technologies</p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Skills;
