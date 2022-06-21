b91ad5f34ba02564e32fe20c6c8fc6f81a34a9ad
JuanP-VT <cv.juanp@gmail.com>
2022-06-21 16:02:26 -0600

    Added content to skill section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,6 +5,13 @@ import {
   faWrench,
   faBookOpenReader,
 } from "@fortawesome/free-solid-svg-icons";
+import jsIcon from "../img/skill-icons/js.png";
+import tsIcon from "../img/skill-icons/ts.png";
+import sqlIcon from "../img/skill-icons/sql.png";
+import htmlIcon from "../img/skill-icons/html.png";
+import cssIcon from "../img/skill-icons/css.png";
+import sassIcon from "../img/skill-icons/sass.png";
+
 function Skills() {
   return (
     <section id="Skills">
@@ -18,6 +25,33 @@ function Skills() {
         <div className="skillBox">
           <FontAwesomeIcon icon={faCode} />
           <p className="title">Languages</p>
+          <div className="skillContainer">
+            <ul>
+              <li>
+                Javascript <img src={jsIcon} alt="javascript icon" />
+              </li>
+              <li>
+                Typescript
+                <img src={tsIcon} alt="typescript icon" />
+              </li>
+              <li>
+                SQL
+                <img src={sqlIcon} alt="SQL icon" />
+              </li>
+              <li>
+                HTML
+                <img src={htmlIcon} alt="HTML icon" />
+              </li>
+              <li>
+                CSS
+                <img src={cssIcon} alt="CSS icon" />
+              </li>
+              <li>
+                SASS
+                <img src={sassIcon} alt="SASS Icon" />
+              </li>
+            </ul>
+          </div>
         </div>
         <div className="skillBox">
           <FontAwesomeIcon icon={faBookOpenReader} />
