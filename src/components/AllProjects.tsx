0f98abfccc74d62cca57c0f3ca4f9d39875a52f8
JuanP-VT <cv.juanp@gmail.com>
2022-06-25 15:25:28 -0600

    added projects section

--- src/components/AllProjects.tsx
+++ src/components/AllProjects.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import ProjectCard from "./ProjectCard";
 
 function AllProjects() {
-  return <section id="AllProjects">AllProjects</section>;
+  return (
+    <section id="AllProjects">
+      <div className="sectionTitle">
+        <p>
+          All My
+          <span className="alter">Projects</span>
+        </p>
+      </div>
+      <div id="projectContainer">
+        <ProjectCard />
+      </div>
+    </section>
+  );
 }
 
 export default AllProjects;
---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+function ProjectCard({}: Props) {
+  return <div className="projectCard">ProjectCard</div>;
+}
+
+export default ProjectCard;
